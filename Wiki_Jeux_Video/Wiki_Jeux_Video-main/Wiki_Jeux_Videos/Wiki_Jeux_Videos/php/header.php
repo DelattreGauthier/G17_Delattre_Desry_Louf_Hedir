a70bfe5aa99c226407de10705662e4810a04da38
DelattreGauthier <144245075+DelattreGauthier@users.noreply.github.com>
2024-03-15 11:50:07 +0100

    V3

---
+++ Wiki_Jeux_Video/Wiki_Jeux_Video-main/Wiki_Jeux_Videos/Wiki_Jeux_Videos/php/header.php
@@ -0,0 +1,39 @@
+<header>
+		  <!-- Titre du site et navigation -->
+		  <h1><a href="../Accueil/accueil.html">Wiki Jeux Vidéo</a></h1>
+		  <nav>
+				<!-- Navigation : Liste des types de jeux et sélecteur de langue -->
+				<ul class="menu">
+				  <li>
+						<a class="liste" href="../Accueil/accueil.html">Types de Jeux <span>&#9662;</span></a>
+						<ul class="submenu">
+						  <!-- Liste déroulante des catégories de jeux -->
+						  <li><a href="../Aventures/aventures.html">Jeux d'Aventures</a></li>
+						  <li><a href="../Simulation/simulation.html">Jeux de Simulation</a></li>
+						  <li><a href="../FPS/fps.html">FPS</a></li>
+						  <li><a href="../Arcade/arcade.html">Jeux d'Arcade</a></li>
+						  <li><a href="../Enigme/enigme.html">Jeux d'Énigmes</a></li>
+						  <li><a href="../Plateforme/plateforme.html">Jeux de Plateformes</a></li>
+						</ul>
+				  </li>
+				</ul>
+				<!-- Sélecteur de langue -->
+				<ul class="language">
+				  <li>
+						<a class="liste2" href="../Accueil/accueil_EN.html">
+						  <div class="photo_language">
+							<img src="../../photo/FonctionnementduSite/photo-language.png" alt="Language">
+						  </div>
+						</a>
+				  </li>
+				</ul>
+
+				<div class="connexion">
+					<a class="connexion-button">Se Connecter</a>
+					<a class="sign-button">S'inscrire</a>
+				</div>
+
+		  </nav>
+		</header>
+
+Ceci est mon header, je veux l'implémenter une fois par page de mon site, rédige moi la page "header.php" et rédige moi le code que je doit inclure dans toute mes pages
