fd2f654623b826f89d9dc4a2e07a5c0e02d750bb
Loïc DESRY <loic.desry@student.junia.com>
2024-04-20 18:35:37 +0200

    V19

--- Wiki_Jeux_Video/Wiki_Jeux_Video-main/Wiki_Jeux_Videos/Wiki_Jeux_Videos/html/Profil/profil.php
+++ Wiki_Jeux_Video/Wiki_Jeux_Video-main/Wiki_Jeux_Videos/Wiki_Jeux_Videos/html/Profil/profil.php
@@ -1,3 +1,25 @@
+<?php
+	session_start();
+	if(isset($_SESSION["authentifie"])&& $_SESSION["authentifie"]==true){
+		$courriel = $_SESSION["nom"];
+		try{
+			// Connexion à la base de données
+			require("../Connexion/connexion.php");  
+			// Requête SQL préparée pour éviter les injections SQL
+			$reqPrep = "SELECT Nom, Prenom, Courriel, Telephone, Pseudonyme FROM account WHERE Courriel = :courriel";
+			// Exécution de la requête avec un paramètre lié
+			$req = $conn->prepare($reqPrep);
+			$reqTab = array(":courriel" => $courriel);
+			$req->execute($reqTab);
+			$resultats = $req->fetchAll(PDO::FETCH_ASSOC);//récupérer le résultat
+			$conn = null; // On ferme la connexion        
+		
+			echo"tet";
+		} catch(Exception $e){
+			die("Erreur : " . $e->getMessage());
+		}
+	}
+	?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -22,6 +44,17 @@
 
 		<!-- Contenu principal -->
 		<main>
+			<div class="grid-item eltA">
+
+			<?php
+			// Affichage des informations de l'utilisateur avec échappement des sorties
+				echo "<h1>Profil de ".$resultats[0]["Pseudonyme"]."</h1>";
+				echo "<h3>Nom :".$resultats[0]["Nom"]."</h3>";
+				echo "<h3>Prenom :".$resultats[0]["Prenom"]."</h3>";
+				echo "<h3>Adresse :".$resultats[0]["Courriel"]."</h3>";
+				echo "<h3>Numéro de téléphone :".$resultats[0]["Telephone"]."</h3>";
+			?>
+            </div>			
 		</main>
 
 		<!-- Pied de page -->
